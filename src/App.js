895e47f967344a6df2a74b89f15e021680f6a07f
sumeetwd25 <sumeetwd25@gmail.com>
2023-11-02 18:07:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import ProductList from './components/ProductList';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ProductDetails from './components/ProductDetails';
 
 function App() {
   return <Routes>
+
     <Route path="/" element={<ProductList></ProductList>}></Route>
     <Route path="/details" element={<ProductDetails></ProductDetails>}></Route>
 
